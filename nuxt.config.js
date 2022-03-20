f0049ae9cca5971b308f64df4118c930c78ac21a
Pawel Pasek <pasekpasek@gmail.com>
2022-03-20 13:26:10 +0100

    Setup vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
-{
-    buildModules: [
-      ['@nuxtjs/vuetify', { /* module options */ }],
-      '@nuxtjs/composition-api/module'
-    ]
-  }
+export default {
+  buildModules: [
+    '@nuxtjs/vuetify',
+    '@nuxtjs/composition-api/module',
+    '@nuxt/typescript-build'
+  ],
+  css: [
+    '~/assets/css/main.scss'
+  ]
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1>Hello world!</h1>
+  <v-app>
+    <v-navigation-drawer app>
+    <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+    <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view />
+        <v-btn @click="counter++">
+          Click me {{ counter }}
+        </v-btn>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+    <!-- -->
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup () {
+    const counter = ref(0)
+    return { counter }
+  }
+})
+</script>
